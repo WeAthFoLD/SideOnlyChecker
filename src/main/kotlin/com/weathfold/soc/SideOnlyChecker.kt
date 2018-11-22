3f81014ce6d349f974c4b99230e8fb9e794109a4
WeAthFolD <weathfold@li-dev.cn>
2018-11-23 01:13:02 +0800

    Mark SideOnly annotations using PsiFile

--- src/main/kotlin/com/weathfold/soc/SideOnlyChecker.kt
+++ src/main/kotlin/com/weathfold/soc/SideOnlyChecker.kt
@@ -1,12 +1,24 @@
 package com.weathfold.soc
 
+import com.intellij.codeInsight.AnnotationUtil
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.compiler.*
 import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.module.impl.scopes.ModulesScope
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.searches.AllClassesSearch
 import java.io.DataInput
 
 
@@ -25,9 +37,41 @@ class SOCRegistration(val p: Project) : ProjectComponent {
             override fun createValidityState(`in`: DataInput): ValidityState = TimestampValidityState.load(`in`)
 
             override fun getProcessingItems(context: CompileContext): Array<FileProcessingCompiler.ProcessingItem> {
-                notifyInfo("Compile", "getProcessingItems")
-                notifyInfo("Files", context.compileScope.getFiles(null, true).map { it.name }.joinToString())
-                return emptyArray()
+                val items = context.compileScope.getFiles(null, true).map { object : FileProcessingCompiler.ProcessingItem {
+                    override fun getValidityState(): ValidityState? {
+                        return EmptyValidityState()
+                    }
+
+                    override fun getFile(): VirtualFile {
+                        return it.canonicalFile!!
+                    }
+
+                } }.toTypedArray<FileProcessingCompiler.ProcessingItem>()
+
+                val psiManager = PsiManager.getInstance(p)
+                items
+                    .mapNotNull {
+                        psiManager.findFile(it.file)
+                    }
+                    .filter { it.fileType is LanguageFileType }
+                    .forEach {
+                        when (it) {
+                            is PsiJavaFile -> {
+                                val javaFile = it
+                                javaFile.classes.forEach {
+                                    val annotations = it.annotations.filter { it.qualifiedName!! == "net.minecraftforge.fml.relauncher.SideOnly" }
+                                    val document = PsiDocumentManager.getInstance(p).getDocument(javaFile)!!
+                                    annotations.forEach {
+                                        context.addMessage(CompilerMessageCategory.ERROR, "Invalid SideOnly usage", javaFile.virtualFile.url,
+                                            document.getLineNumber(it.textOffset) + 1, 0)
+                                    }
+                                }
+                            }
+                            else -> {}
+                        }
+                    }
+
+                return items
             }
 
             override fun validateConfiguration(scope: CompileScope): Boolean {
@@ -39,7 +83,6 @@ class SOCRegistration(val p: Project) : ProjectComponent {
             }
 
             override fun process(context: CompileContext, items: Array<FileProcessingCompiler.ProcessingItem>): Array<FileProcessingCompiler.ProcessingItem> {
-                notifyInfo("Compile", "process")
                 return items
             }
 
